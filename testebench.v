d6c005315e326d911419184dbe9c6be349a15081
Vinícius <omendes.vinicius@gmail.com>
2021-03-25 00:12:08 -0300

    commit on main

--- testebench.v
+++ testebench.v
@@ -2,11 +2,11 @@
 
 module testbench ();
 
-reg C1 = 0;
-reg C2 = 0;
-reg C3 = 0;
-reg C4 = 0;
-reg C5 = 0;
+reg C1 = 1;
+reg C2 = 1;
+reg C3 = 1;
+reg C4 = 1;
+reg C5 = 1;
 
 wire A;
 wire B;
@@ -21,6 +21,41 @@ display meumoduloTeste(C1,C2,C3,C4,C5,A,B,C,D,E,F,G);
 initial begin
     $dumpfile("testbench.vcd");
     $dumpvars;
+
+    #1
+    C1 = 1;
+    C2 = 0;
+    C3 = 1;
+    C4 = 1;
+    C5 = 1;
+
+    #1
+    C1 = 1;
+    C2 = 1;
+    C3 = 0;
+    C4 = 1;
+    C5 = 0;    
+
+    #1
+    C1 = 0;
+    C2 = 1;
+    C3 = 0;
+    C4 = 0;
+    C5 = 1;
+
+    #1
+    C1 = 0;
+    C2 = 0;
+    C3 = 0;
+    C4 = 1;
+    C5 = 0;
+
+    #1
+    C1 = 0;
+    C2 = 0;
+    C3 = 0;
+    C4 = 0;
+    C5 = 0;
 
     #100;
     $finish;
